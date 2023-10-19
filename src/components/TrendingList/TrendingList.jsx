332e2010431c47365c5ff708926bd9fa0803f782
Ваня <7328652@gmail.com>
2023-10-19 13:49:19 +0300

    done

---
+++ src/components/TrendingList/TrendingList.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getTrendingMovies } from 'services/api';
+import { defaultImg } from 'services/imgDefault';
+import { MovieLink, Title, TrendingTodayList } from './TrendingList.styled';
+
+function TrendingList() {
+  const [movies, setMovies] = useState([]);
+
+  const location = useLocation();
+  useEffect(
+    () => async () => {
+      try {
+        const data = await getTrendingMovies();
+        setMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [movies]
+  );
+  return (
+    <>
+      <main>
+        <div className="container">
+          <Title>Trending today</Title>
+          <TrendingTodayList>
+            {movies.results?.map(({ id, poster_path, title }) => (
+              <li key={id}>
+                <MovieLink to={`movies/${id}`} state={{ from: location }}>
+                  <img
+                    src={
+                      poster_path
+                        ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+                        : defaultImg
+                    }
+                    alt={title}
+                    width={264}
+                    height={396}
+                  />
+                  <h3>{title}</h3>
+                </MovieLink>
+              </li>
+            ))}
+          </TrendingTodayList>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default TrendingList;
